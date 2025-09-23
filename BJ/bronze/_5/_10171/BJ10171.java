e12ac672375aea7c56abc352ea100d6015e1f464
피우리 <piwoorii@gmail.com>
2025-09-23 15:05:02 +0900

    feat: add BJ10171 solution

---
+++ BJ/bronze/_5/_10171/BJ10171.java
@@ -0,0 +1,10 @@
+package BJ.bronze._5._10171;
+
+public class BJ10171 {
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    } 
+}
