c2869bc873027c2ba9fadc2be7f374fcb963f0f9
피우리 <piwoorii@gmail.com>
2025-09-27 15:14:28 +0900

    feat: add BJ11382 solution

---
+++ BJ/bronze/_5/_11382/BJ11382.java
@@ -0,0 +1,13 @@
+package BJ.bronze._5._11382;
+import java.util.Scanner;
+
+public class BJ11382 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+        System.out.println(a + b + c);
+        sc.close();
+    }
+}
