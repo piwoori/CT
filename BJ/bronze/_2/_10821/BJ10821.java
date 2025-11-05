2b452d5a4a91196d8884f03b85fb060d41e13159
피우리 <piwoorii@gmail.com>
2025-11-05 14:20:06 +0900

    feat: add bronze2/BJ10821 solution

---
+++ BJ/bronze/_2/_10821/BJ10821.java
@@ -0,0 +1,13 @@
+package BJ.bronze._2._10821;
+
+import java.util.Scanner;
+public class BJ10821 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        String[] list = S.split(",");
+        System.out.println(list.length);
+        sc.close();
+    }
+    
+}
