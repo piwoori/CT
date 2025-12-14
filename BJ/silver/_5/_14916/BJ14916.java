bf9a7b327d86eda5cc4b087d7185eef3d8d2d3d5
피우리 <piwoorii@gmail.com>
2025-12-14 14:52:15 +0900

    feat: add silver5/BJ14916 solution

---
+++ BJ/silver/_5/_14916/BJ14916.java
@@ -0,0 +1,28 @@
+package BJ.silver._5._14916;
+
+import java.util.Scanner;
+
+public class BJ14916 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int count = 0;
+        int temp = 0;
+
+        while(true){
+            if (N % 5 == 0){
+                count += N / 5;
+                System.out.println(count);
+                break;
+            } else{
+                N -= 2;
+                count++;
+            }
+
+            if(N < 0){
+                System.out.println(-1);
+                break;
+            }
+        }
+    }
+}
