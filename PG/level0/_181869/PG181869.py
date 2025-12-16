d4741816974a06103fe0d5308f9ae2a8c35e81ca
피우리 <piwoorii@gmail.com>
2025-12-16 21:57:47 +0900

    feat: add level0/PG181869  solution

---
+++ PG/level0/_181869/PG181869.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = []
+    for i in my_string.split(" "):
+        answer.append(i)
+    return answer
+
+def solution(my_string):
+    return my_string.split(' ')
