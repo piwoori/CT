0f84d8cb6d02f9289b3d0ece35158e2659c9d40e
피우리 <piwoorii@gmail.com>
2026-01-05 13:40:18 +0900

    feat: add level0/PG181851 solution

---
+++ PG/level0/_181851/PG181851.py
@@ -0,0 +1,9 @@
+def solution(rank, attendance):
+    count = 0
+    answer = []
+    for i in range(len(rank)):
+        if attendance[i]:
+            answer.append([rank[i], i])
+
+    answer.sort()
+    return answer[0][1] * 10000 + answer[1][1] * 100 + answer[2][1]
