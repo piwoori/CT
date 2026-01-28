cd0f963dc748c30ec3ffd96bfe6f57f934d63382
피우리 <piwoorii@gmail.com>
2026-01-28 13:06:00 +0900

    feat: add silver4/BJ2839 solution

---
+++ BJ/silver/_4/_2839/BJ2839.java
@@ -0,0 +1,23 @@
+package BJ.silver._4._2839;
+
+import java.io.*;
+
+public class BJ2839 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N == 4 || N == 7) {
+            System.out.println(-1);
+        }
+        else if (N % 5 == 0) {
+            System.out.println(N / 5);
+        }
+        else if (N % 5 == 1 || N % 5 == 3) {
+            System.out.println((N / 5) + 1);
+        }
+        else if (N % 5 == 2 || N % 5 == 4) {
+            System.out.println((N / 5) + 2);
+        }
+    }
+}
