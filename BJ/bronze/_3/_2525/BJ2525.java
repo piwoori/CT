fd5804629de1e44426c43675089a51e9aaee248f
피우리 <piwoorii@gmail.com>
2026-02-11 18:09:00 +0900

    feat: add bronze3/BJ2525 solution

---
+++ BJ/bronze/_3/_2525/BJ2525.java
@@ -0,0 +1,27 @@
+package BJ.bronze._3._2525;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ2525 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int C = Integer.parseInt(st.nextToken());
+
+        int total = A * 60 + B + C;
+
+        if(total > 1440){
+            total -= 1440;
+        }
+
+        A = total / 60;
+        B = total % 60;
+
+        System.out.println(A + " " + B);
+    }
+}
