8d0cb05ae46ae96c2c4c1b4cf96aa3b934255a48
피우리 <piwoorii@gmail.com>
2026-03-05 12:47:17 +0900

    feat: add bronze2/BJ1152 solution

---
+++ BJ/bronze/_2/_1152/BJ1152.java
@@ -0,0 +1,14 @@
+package BJ.bronze._2._1152;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class BJ1152 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        System.out.println(st.countTokens());
+    }
+}
