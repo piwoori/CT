a13fa1e47d4977c7d75ef5f21eb6365f9f3177ec
피우리 <piwoorii@gmail.com>
2026-03-09 16:59:25 +0900

    feat: add bronze5/BJ25083 solution

---
+++ BJ/bronze/_5/_25083/BJ25083.java
@@ -0,0 +1,12 @@
+package BJ.bronze._5._25083;
+
+public class BJ25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
