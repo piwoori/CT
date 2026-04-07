1e5fa26701fe633154dcab29ca872a714a059ceb
피우리 <piwoorii@gmail.com>
2026-04-07 16:53:09 +0900

    feat: add bronze5/BJ24263 solution

---
+++ BJ/bronze/_5/_24263/BJ24263.java
@@ -0,0 +1,14 @@
+package BJ.bronze._5._24263;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ24263 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(N);
+        System.out.println(1);
+    }
+}
