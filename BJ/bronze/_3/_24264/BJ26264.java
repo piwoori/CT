82056a2482694c500fcec98f5e0bd7b977119b17
피우리 <piwoorii@gmail.com>
2026-04-08 10:50:36 +0900

    feat: add bronze3/BJ24264 solution

---
+++ BJ/bronze/_3/_24264/BJ26264.java
@@ -0,0 +1,14 @@
+package BJ.bronze._3._24264;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ26264 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(((long) N * N));
+        System.out.println(2);
+    }
+}
