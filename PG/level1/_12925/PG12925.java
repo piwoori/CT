7e57879fd0fc1d6561259a5db4942dae14486839
피우리 <piwoorii@gmail.com>
2026-04-26 10:49:27 +0900

    feat: add level1/PG12925 solution

---
+++ PG/level1/_12925/PG12925.java
@@ -0,0 +1,16 @@
+package PG.level1._12925;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PG12925 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        int answer = Integer.parseInt(br.readLine());
+
+        System.out.println(answer);
+
+    }
+}
