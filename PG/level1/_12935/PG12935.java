1bc88d91742ec7124623bd4e124b0e497d90c79f
피우리 <piwoorii@gmail.com>
2026-05-14 13:29:09 +0900

    feat: add level1/_12935 solution

---
+++ PG/level1/_12935/PG12935.java
@@ -0,0 +1,29 @@
+package PG.level1._12935;
+
+import java.util.*;
+
+public class PG12935 {
+    public int[] solution(int[] arr){
+
+        if(arr.length == 1){
+            return new int[]{-1};
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for(int num : arr){
+            min = Math.min(min, num);
+        }
+
+        int [] answer = new int[arr.length - 1];
+        int idx = 0;
+
+        for(int num : arr){
+            if(num != min){
+                answer[idx++] = num;
+            }
+        }
+
+        return answer;
+    }
+}
