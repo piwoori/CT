ae0d550ea822bdb81774eec9fad8e922d0d4796d
피우리 <piwoorii@gmail.com>
2026-06-18 09:03:06 +0900

    feat: add level1/_135808 solution

---
+++ PG/level1/_135808/PG135808.java
@@ -0,0 +1,17 @@
+package PG.level1._135808;
+
+import java.util.Arrays;
+
+public class PG135808 {
+    public int solution(int k, int m, int[] score){
+        int answer = 0;
+
+        Arrays.sort(score);
+
+        for(int i = score.length - m; i >= 0; i -= m){
+            answer += score[i] * m;
+        }
+
+        return answer;
+    }
+}
