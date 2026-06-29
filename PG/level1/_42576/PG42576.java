02f943b72d11e47cdaf8ea52d7fa4f3a802fc1db
피우리 <piwoorii@gmail.com>
2026-06-29 21:25:54 +0900

    feat: add level1/_42576 solution

---
+++ PG/level1/_42576/PG42576.java
@@ -0,0 +1,26 @@
+package PG.level1._42576;
+
+import java.util.HashMap;
+
+public class PG42576 {
+    public String solution(String[] participant, String[] completion){
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(String p : participant){
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+
+        for(String c : completion){
+            map.put(c, map.get(c) - 1);
+        }
+
+        for(String key : map.keySet()){
+            if(map.get(key) > 0){
+                return key;
+            }
+        }
+
+        return "";
+
+    }
+}
