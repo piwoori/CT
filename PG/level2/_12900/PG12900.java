b3f7c8f45c8c97f2bce497265f633b73655bfd27
피우리 <piwoorii@gmail.com>
2026-08-28 21:39:25 +0900

    feat: add level2/_12900 solution

---
+++ PG/level2/_12900/PG12900.java
@@ -0,0 +1,19 @@
+package PG.level2._12900;
+
+public class PG12900 {
+    public int solution(int n) {
+        int[] dp = new int[n + 1];
+
+        dp[1] = 1;
+
+        if(n >= 2){
+            dp[2] = 2;
+        }
+
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+
+        return dp[n];
+    }
+}
